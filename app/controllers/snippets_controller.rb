9b78741947493ff76226670372be972146d2e892
Patrick Smith <patrick@burntcaramel.com>
2017-04-13 12:05:12 +1000

    Add creation of snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -6,4 +6,19 @@ class SnippetsController < ApplicationController
   def new
     @snippet = Snippet.new
   end
+
+  def create
+    # Get the submitted form data from params
+    snippet_params = params.require(:snippet).permit(:title, :description, :code)
+    # Create a new model object with that data
+    @snippet = Snippet.new(snippet_params)
+    # Attempt to save that model to the database
+    if @snippet.save
+      # If saved, then redirect to that snippet’s page
+      redirect_to @snippet, notice: 'Your snippet was created'
+    else
+      # Otherwise, show the user the form again with errors
+      render :new
+    end
+  end
 end
